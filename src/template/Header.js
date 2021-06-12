02d910e9c5adf166a242b476bfeb85c9f73eccb6
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2021-06-12 09:12:35 -0300

    Añade goBack()

--- src/template/Header.js
+++ src/template/Header.js
@@ -6,7 +6,7 @@ class Header extends React.Component {
         return (
             <nav className="container-fluid navbar navbar-dark bg-dark">
                 <div className="container">
-                    <div className="navbar-brand" onClick={()=> Router.back()}>Lista de Pacientes</div>
+                    <div className="navbar-brand" onClick={()=> Router.goBack()}>Lista de Pacientes</div>
                     
                 </div>
             </nav>
